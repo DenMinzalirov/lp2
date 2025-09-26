6f2a87152fc87c7f6d64991b91a55367006d32aa
Dzianis Minzakirau <denis@fideum.group>
2025-09-26 12:36:43 +0300

    clarity-script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import Script from 'next/script'
 import './globals.css'
 
 export const metadata: Metadata = {
@@ -13,6 +14,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ru">
+      <head>
+        <Script
+          id="clarity-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "tf8nx2yacw");
+            `,
+          }}
+        />
+      </head>
       <body className="bg-gray-900 m-0 p-0">
         {children}
       </body>
